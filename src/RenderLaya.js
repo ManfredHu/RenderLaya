6da88735e0fe9fac7cb00fe0e9d50b85d18b13be
ManfredHu <279331641@qq.com>
2018-04-20 11:27:53 +0800

    Update RenderLaya.js

--- src/RenderLaya.js
+++ src/RenderLaya.js
@@ -7,7 +7,7 @@
 /**
  * The MIT License (MIT)
  * 
- * Copyright (c) Liam Brummitt and contributors.
+ * Copyright (c) ManfredHu.
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
